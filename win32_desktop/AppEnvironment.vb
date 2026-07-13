ae7729b45834db67cdcc62275e298b384db494ad
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-14 02:01:33 +0800

    try to config of the web environment

---
+++ win32_desktop/AppEnvironment.vb
@@ -0,0 +1,36 @@
+﻿Module AppEnvironment
+
+    Public ReadOnly Property globalHttpPort As Integer
+        Get
+            If http Is Nothing Then
+                Return 0
+            Else
+                Return http.port
+            End If
+        End Get
+    End Property
+
+    Dim http As HttpServices
+
+    Public Function CheckDevelopmentMode() As Boolean
+        Dim home As String = App.HOME.GetDirectoryFullPath
+        Dim check = home.StartsWith("G:/VirtualViewer")
+
+        Return check
+    End Function
+
+    Public Function GetWwwRoot() As String
+        If AppEnvironment.CheckDevelopmentMode Then
+            Return "G:\VirtualViewer\src\viewer"
+        Else
+            Return App.HOME & "/app"
+        End If
+    End Function
+
+    Public Function StartGlobalHttp() As Integer
+        http = New HttpServices(GetWwwRoot)
+        http = http.StartHttp
+        Return http.port
+    End Function
+
+End Module
