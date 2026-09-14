33d0bb2ffd39bb9e2e06a63900e2b18c2fcdae2f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-15 00:54:47 +0800

    removes obsolste project source

--- src/CFD/Globals.vb
+++
@@ -1,8 +0,0 @@
-﻿Module Globals
-
-    ' Calculation short-cuts:
-    Public Const four9ths As Double = 4.0 / 9
-    Public Const one9th As Double = 1.0 / 9
-    Public Const one36th As Double = 1.0 / 36
-
-End Module
